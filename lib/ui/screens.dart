47c1fbce803d37e186cfeb97fedcf142d5d3c29d
Nguyen Minh Nhut <nhutb2205896@student.ctu.edu.vn>
2025-11-09 20:23:48 +0700

    modify export

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -1,24 +1,18 @@
 // Auth
 export 'auth/auth_manager.dart';
-export 'auth/auth_card.dart';
 export 'auth/login_screen.dart';
 export 'auth/signup_screen.dart';
 
 // Account
-export 'account/account_screen.dart';
 export 'account/account_manager.dart';
-export 'account/edit_profile_screen.dart';
 
 // Categories
-export 'categories/categories_screen.dart';
 export 'categories/categories_manager.dart';
-export 'categories/edit_category_screen.dart';
+
 
 // Transactions
-export 'transactions/transactions_screen.dart';
 export 'transactions/transactions_manager.dart';
-export 'transactions/add_transaction_screen.dart';
-export 'transactions/edit_transaction_screen.dart';
+
 
 // Home
 export 'home/home_screen.dart';
@@ -32,6 +26,4 @@ export 'bottom_nav_bar_screen.dart';
 
 // Savings Goals
 export 'savings_goals/savings_goals_manager.dart';
-export 'savings_goals/savings_goals_block.dart';
-export 'savings_goals/savings_goals_screen.dart';
-export 'savings_goals/add_edit_goal_screen.dart';
+
