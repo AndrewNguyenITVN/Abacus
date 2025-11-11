3a9371e0207c2db919279496c2e8ecf86dab1c69
Nguyen Minh Nhut <nhutb2205896@student.ctu.edu.vn>
2025-11-11 20:22:15 +0700

    style: delete dummy data

--- lib/ui/transactions/transactions_manager.dart
+++ lib/ui/transactions/transactions_manager.dart
@@ -7,54 +7,7 @@ class TransactionsManager extends ChangeNotifier {
 
   final List<Transaction> _transactions = [];
 
-  TransactionsManager(this.categoriesManager) {
-    final now = DateTime.now();
-    final expenseCategories = categoriesManager.expenseCategories;
-    final incomeCategories = categoriesManager.incomeCategories;
-    _transactions.addAll([
-      Transaction(
-        id: 't1',
-        amount: 65000,
-        description: 'Bữa trưa',
-        date: now.subtract(const Duration(days: 1)),
-        categoryId: expenseCategories[0].id,
-        type: 'expense',
-        note: 'Ăn cùng bạn bè',
-      ),
-      Transaction(
-        id: 't2',
-        amount: 1500000,
-        description: 'Mua sắm',
-        date: now.subtract(const Duration(days: 2)),
-        categoryId: expenseCategories[1].id,
-        type: 'expense',
-      ),
-      Transaction(
-        id: 't3',
-        amount: 10000000,
-        description: 'Lương tháng này',
-        date: now,
-        categoryId: incomeCategories[0].id,
-        type: 'income',
-      ),
-      Transaction(
-        id: 't4',
-        amount: 500000,
-        description: 'Tiền xăng xe',
-        date: now.subtract(const Duration(hours: 5)),
-        categoryId: expenseCategories[2].id,
-        type: 'expense',
-      ),
-       Transaction(
-        id: 't5',
-        amount: 2000000,
-        description: 'Tiền thưởng',
-        date: now.subtract(const Duration(days: 5)),
-        categoryId: incomeCategories[1].id,
-        type: 'income',
-      ),
-    ]);
-  }
+  TransactionsManager(this.categoriesManager);
 
   List<Transaction> get transactions {
     _transactions.sort((a, b) => b.date.compareTo(a.date));
